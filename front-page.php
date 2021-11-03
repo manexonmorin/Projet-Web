60176ee257a77514fde565bdcb0c52606207580e
Finder-00 <arthurrobitaille@protonmail.com>
2021-11-03 14:10:57 -0400

    footer, accueil etc

--- front-page.php
+++ front-page.php
@@ -19,7 +19,6 @@ get_header();
 	<main id="primary" class="site-main">
 
   <!-- Balise div qui contient les particules à faire bouger -->
-    <!-- Balise div qui contient les particules à faire bouger -->
     <div id="particle-container">
       <div class="particle"></div>
       <div class="particle"></div>
@@ -52,21 +51,6 @@ get_header();
       <div class="particle"></div>
       <div class="particle"></div>
     </div>
-
-      <!-- Balise qui contient les Bulles à effet flottant -->
-    <!-- <div id="bulleAnim" class="lesbulles">
-
-      <div class="bulleIntera">
-          <h1 class="titreBulle"> <span class="effetGras-bulle1" >T</span>echnique <br> <span class="effetGras-bulle1" >I</span>ntégration <br> <span class="effetGras-bulle1" >M</span>ultimédia </h1>
-      </div>
-
-      <div class="bulleIntera bulleIntera2">
-        <h1 class="titreBulle titreBulle2">BIENVENUE</h1>
-      </div>
-
-      <div class="bulleIntera bulleIntera3">
-      </div>
-    </div> -->
 
 		<?php
 		if ( have_posts() ) :
@@ -102,33 +86,6 @@ get_header();
 		endif;
 		?>
 	</main><!-- #main -->
-
-  <!-- ************************************************************* SECTION CAMMADERIE APPERCU ACCUEIL ************************************************************* -->
-  <!-- <section class="un cammaderie">
-    <div class="conteneur_cammaderie">
-      <h1 class="titre_cammaderie" >CAMMADERIE</h1>
-      <p class="text_cammaderie">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor <br>
-      invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et d diam nonumy <br> 
-      eirmod tempor invidunt ut labore et dolore magnajv deg mi sola de tiuid. Demmo Aden 
-      </p>
-    </div>
-  </section> -->
-
-  <!-- ******************************************************** SECTION PROJET ETUDIANT APPERCU ACCUEIL ************************************************************* -->
-  <!-- <section class="projet_etudiant" id="projet_etudiant">
-
-  <div id="particle-container">
-<section class="un cammaderie"> -->
-    <!-- <div class="conteneur_cammaderie"> -->
-      <!-- <h1 class="titre_cammaderie" >CAMARADERIE</h1> -->
-      <!-- <p class="text_cammaderie">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor <br>
-      invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et d diam nonumy <br> 
-      eirmod tempor invidunt ut labore et dolore magnajv deg mi sola de tiuid. Demmo Aden 
-      </p> -->
-    <!-- </div> -->
-  <!-- </section> -->
 
   <!-- ******************************************************** SECTION PROJET ETUDIANT APPERCU ACCUEIL ************************************************************* -->
   <section class="projet_etudiant" id="projet_etudiant">
@@ -166,77 +123,11 @@ get_header();
       <div class="particle"></div>
     </div>
 
-    <div class="conteneur_titre-etudiant">
-    </div> 
-
-    <!-- <div class="conteneur_titre-etudiant">
-
-      <h1 class="titre_projet-etudiant">PROJET <br> ETUDIANT</h1>
-
-      <div class="bulleIntera bulle_projet">
-        <a class="lien_projet" href="#">Voir tout les projets</a>
-      </div>
-
-    </div>
-
-    <div class="conteneur_bulle_projet">
-
-      <div class="conteneur_projet projet-un"></div>
-      <div class="conteneur_projet projet-deux"></div>
-      <div class="conteneur_projet projet-trois"></div>
-      <div class="conteneur_projet projet-quatre"></div>
-      <div class="conteneur_projet projet-cinq"></div>
-
-    </div> -->
   </section>
-
-  <!-- ******************************************************** SECTION PROJET ETUDIANT APPERCU ACCUEIL ************************************************************* -->
-
-    <!-- <section class="language_logiciel">
-        <h1 class="titre_language_logiciel">LANGUAGES & <br> LOGICIELS</h1>
-
-      <div class="conteneur_logiciel_language">
-
-        <div class="bulleIntera logiciel-1 bulle_logiciel">
-          <div class="sous_logi enfant3"></div>
-          <div class="sous_logi enfant4"></div>
-        </div>
-
-        <div class="bulleIntera logiciel-2 bulle_logiciel">
-          <div class="sous_logi enfant1"></div>
-          <div class="sous_logi enfant2"></div>
-        </div>
-
-        <div class="bulleIntera logiciel-3 bulle_logiciel">
-          <div class="sous_logi enfant5"></div>
-          <div class="sous_logi enfant6"></div>          
-        </div>
-
-        <div class="bulleIntera logiciel-4 bulle_logiciel">
-          <div class="sous_logi enfant7"></div>
-          <div class="sous_logi enfant8"></div>
-        </div>
-
-        <div class="bulleIntera logiciel-5 bulle_logiciel">
-          <div class="sous_logi enfant9"></div>
-          <div class="sous_logi enfant10"></div>
-        </div>
-
-      </div>
-    </section> -->
-
-    <!-- ******************************************************** SECTION FUTURE APPERCU ACCUEIL ************************************************************* -->
-
-    <!-- <section class="avenir">
-
-    <section class="avenir">
-
-    </section> -->
 
 <?php
 get_sidebar();
 get_footer();
-
 
 
 function affichageAccueil(&$tPropriete){
