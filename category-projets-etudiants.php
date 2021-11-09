25aef1809f7bb5d134e0dc3299608f8a04d393a6
Finder-00 <arthurrobitaille@protonmail.com>
2021-11-09 17:15:33 -0500

    mofid category proj etu

--- category-projets-etudiants.php
+++ category-projets-etudiants.php
@@ -6,13 +6,7 @@
 			<header class="page-header">
 			</header><!-- .page-header -->
 			<section class="contenuDesSpectacles">
-			<div class="titrePage">
-				<?php
-					echo '<h1 class="page-title">' . single_cat_title('', false ). '</h1>';
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</div>
-			
+                
 			<?php
 			while ( have_posts() ) :
 				the_post();
